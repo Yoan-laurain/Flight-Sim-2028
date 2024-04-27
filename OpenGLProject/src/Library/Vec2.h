a494b71adacdf6ec0a498cb4d30c1597da68a1b3
Yoan Laurain <yoan.laurain0@gmail.com>
2024-04-27 15:39:43 +0200

    Fix cpu heightmap generation

--- OpenGLProject/src/Library/Vec2.h
+++ OpenGLProject/src/Library/Vec2.h
@@ -8,15 +8,15 @@ struct Vec2
     Vec2() : x(0), y(0) {}
     Vec2(T _x, T _y) : x(_x), y(_y) {}
     Vec2(T _value ) : x(_value), y(_value) { }
-
-    Vec2 operator+(const Vec2& other) const
-    {
-        return Vec2(x + other.x, y + other.y);
-    }
     
     Vec2 operator-(const Vec2& other) const
     {
         return Vec2(x - other.x, y - other.y);
+    }
+
+    Vec2 operator+(const Vec2<int>& other) const
+    {
+        return Vec2(x + other.x, y + other.y);
     }
     
     Vec2 operator*(T scalar) const
--- OpenGLProject/src/Terrain/PerlinNoise/CPU/PerlinNoiseModuleCPU.cpp
+++ OpenGLProject/src/Terrain/PerlinNoise/CPU/PerlinNoiseModuleCPU.cpp
@@ -1,26 +1,65 @@
 ﻿#include "PerlinNoiseModuleCPU.h"
-#include "Library/Math.h"
 #include "Terrain/PerlinNoise/PerlinNoise.h"
+#include "Core/Application.h"
+#include "Terrain/TerrainGenerator.h"
+#include "Library/Vec2.h"
 #include <random>
 
-PerlinNoiseModuleCPU::PerlinNoiseModuleCPU(){}
+PerlinNoiseModuleCPU::PerlinNoiseModuleCPU() = default;
 
 void PerlinNoiseModuleCPU::Process(std::vector<float>& heighmap)
 {
-    std::random_device rd;
-    PerlinNoise perlin = PerlinNoise(rd());
-   
-    int height =  sqrt(heighmap.size());
-    if(height <=0)return;
-    int width = height;
+    heighmap.clear();
+    heighmap.resize(
+        Application::Get()->GetTerrainGenerator()->m_BorderedMapSize * Application::Get()->GetTerrainGenerator()->
+        m_BorderedMapSize);
 
-    for (int y = 0; y < height; ++y) {
-        for (int x = 0; x < width; ++x) {
-            float nx = x*m_Frequency;
-            float ny = y*m_Frequency;
-            
-            int index = y * width + x;
-            heighmap[index] = perlin.noise(nx, ny);
+    bool randomizeSeed = false;
+    const int seed = randomizeSeed ? std::random_device()() : 0;
+    std::mt19937 prng(seed);
+    std::uniform_int_distribution distribution(-1000, 1000);
+
+    PerlinNoise perlin = PerlinNoise(seed);
+
+    int numOctaves = 8;
+    std::vector<Vec2<int>> offsets(numOctaves);
+    for (int i = 0; i < numOctaves; i++)
+    {
+        offsets[i] = {distribution(prng), distribution(prng)};
+    }
+
+    float minValue = std::numeric_limits<float>::max();
+    float maxValue = std::numeric_limits<float>::min();
+
+    int mapSize = Application::Get()->GetTerrainGenerator()->m_BorderedMapSize;
+    float m_Persistence = .52f;
+    float m_Lacunarity = 1.66f;
+
+    for (int y = 0; y < mapSize; y++)
+    {
+        for (int x = 0; x < mapSize; x++)
+        {
+            float noiseValue = 0;
+            float scale = 2;
+            float weight = 1;
+            for (int i = 0; i < numOctaves; i++)
+            {
+                Vec2<float> p = Vec2(x / (float)mapSize, y / (float)mapSize) * scale + offsets[i];
+                noiseValue += perlin.noise(p.x, p.y) * weight;
+                weight *= m_Persistence;
+                scale *= m_Lacunarity;
+            }
+            heighmap[y * mapSize + x] = noiseValue;
+            minValue = std::min(noiseValue, minValue);
+            maxValue = std::max(noiseValue, maxValue);
+        }
+    }
+
+    if (maxValue != minValue)
+    {
+        for (float& i : heighmap)
+        {
+            i = (i - minValue) / (maxValue - minValue);
         }
     }
 }
