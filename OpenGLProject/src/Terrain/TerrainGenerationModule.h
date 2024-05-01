3f5526ebcf83ea3cbdc48a5dcbc43e6840797d9a
Romain Salha <salha.romain64@gmail.com>
2024-05-01 19:43:50 +0200

    Merge Main

--- OpenGLProject/src/Terrain/TerrainGenerationModule.h
+++ OpenGLProject/src/Terrain/TerrainGenerationModule.h
@@ -18,20 +18,14 @@ class TerrainGenerationModule
         double m_MaxGenerationTime = std::numeric_limits<float>::min();
         double m_AverageGenerationTime = 0.f;
         double m_MinGenerationTime = std::numeric_limits<float>::max();
+
+        double GetAverageGenerationTime() const;
+        std::vector<double> m_AverageGenerationTimeHistory;
     
     private:
         bool m_isDirty;
         std::vector<float> m_lastGeneratedHeighmap;
 
-        std::vector<double> m_AverageGenerationTimeHistory;
-
-        double GetAverageGenerationTime() const
-        {
-            double sum = 0;
-            for(const auto& time : m_AverageGenerationTimeHistory)
-            {
-                sum += time;
-            }
-            return sum / m_AverageGenerationTimeHistory.size();
-        }
+        
+    
 };
